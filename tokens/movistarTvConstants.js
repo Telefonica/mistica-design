188b879c415db8f5e2f3192b98d992874d47106f
Yayo <yceballost@gmail.com>
2021-07-09 11:58:48 +0200

    tokens tv

---
+++ tokens/movistarTvConstants.js
@@ -0,0 +1,105 @@
+// Las constantes tienen aplicadas dos variables; primero su versión light y a continuación la dark mode. En las constantes que solo se muestra una variable, esta es compartida por ambos modos.
+
+// BACKGROUNDS
+// appBarBackground = NA
+background = "$black"
+backgroundContainer = "$grey4"
+backgroundBrand = "$movistarBlue" // "$darkModeBlack"
+backgroundOverlay = "$black" /* alpha 80% */
+// backgroundSkeleton = "$grey3"
+// backgroundSkeletonInverse = 
+// navigationBarBackground = 
+// backgroundAlternative = "$grey4"
+// backgroundFeedbackBottom = 
+
+// skeletonWave = 
+
+// BORDERS
+// borderLight = 
+// borderDark = 
+border = "$white" / "$grey3"
+borderSelected = "$movistarBlue"
+
+// BUTTONS
+buttonDangerBackground = "$pepper"
+buttonDangerBackgroundDisabled = "$pepperLight30" // "$darkModeGrey"
+buttonDangerBackgroundSelected = "$pepperDark"
+buttonDangerBackgroundHover = "$pepperDark" // web only
+
+buttonLinkBackgroundSelected = "$movistarBlueLight10" // "$white alpha 0.05"
+buttonLinkBackgroundSelectedInverse = "$white" /* alpha 20% */ // "$white alpha 0.05"
+
+buttonPrimaryBackground = "$movistarGreen" // "$movistarGreen"
+buttonPrimaryBackgroundDisabled = "$movistarGreenLight50" // "$darkModeGrey"
+buttonPrimaryBackgroundDisabledInverse = "$movistarBlueLight50" // "$darkModeGrey"
+buttonPrimaryBackgroundInverse = "$white" // "$movistarGreen"
+buttonPrimaryBackgroundSelected = "$movistarGreenDark" // "$movistarGreenDark"
+buttonPrimaryBackgroundHover = "$movistarGreenDark" // "$movistarGreenDark" // web only
+buttonPrimaryBackgroundSelectedInverse = "$movistarBlueLight50" // "$movistarGreenDark"
+
+buttonSecondaryBackground = "$movistarGreen" // "$movistarGreen"
+buttonSecondaryBackgroundDisabled = "$movistarGreenLight50" // "$darkModeGrey"
+buttonSecondaryBackgroundSelected = "$movistarGreenDark" // "$movistarGreenDark"
+buttonSecondaryBorderDisabledInverse = "$movistarBlueLight50" // "$darkModeGrey"
+buttonSecondaryBorderInverse = "$white" // "$movistarGreen"
+buttonSecondaryBorderSelectedInverse = "$movistarBlueLight50" // "$movistarGreenDark"
+
+textButtonPrimary = "$white" // "$grey2"
+[NEW] textButtonPrimaryDisabled = "$white" // "$grey5"
+textButtonPrimaryInverse = "$movistarBlue" // "$grey2"
+textButtonPrimaryInverseDisabled = "$movistarBlueLight30" // "$grey5"
+textButtonPrimaryInverseSelected = "$movistarBlue" // "$grey2"
+
+textButtonSecondary = "$movistarGreen" // "$grey2"
+textButtonSecondaryDisabled = "$movistarGreenLight50" // "$grey5"
+textButtonSecondarySelected = "$movistarGreenDark" // "$movistarGreenDark"
+textButtonSecondaryInverse = "$white" // "$grey2"
+textButtonSecondaryInverseDisabled = "$movistarBlueLight50" // "$grey5"
+textButtonSecondaryInverseSelected = "$white" // "$movistarGreenDark"
+
+textLink = "$movistarBlue" // "$movistarBlue"
+[NEW] textLinkInverse = "$white" // "$movistarBlue"
+textLinkDanger = "$pepper"
+textLinkDangerDisabled = "$pepperLight30" // "$grey5"
+textLinkDisabled = "$movistarBlueLight50" // "$grey6"
+textLinkSnackbar = "$movistarBlueLight50"
+
+// CONTROLS
+control = "$grey3"
+controlActivated = "$movistarBlue"
+// controlError = "$pepper"
+// loadingBar = 
+// loadingBarBackground =
+
+// toggleAndroidInactive = 
+// toggleAndroidBackgroundActive = 
+controlKnob = "$white"
+
+// DIVIDERS  
+divider = "$white" 
+// dividerInverse =
+// navigationBarDivider =
+
+// FEEDBACKS  
+badge = "$pepper" 
+
+// GLOBAL  
+brand = "$movistarBlue"
+// brandDark = 
+inverse = "$white"
+neutralHigh = "$grey4"
+neutralMedium = "$grey2"
+neutralLow = "$grey1"
+promo = "$purple"
+highlight = "$pink"
+
+textPrimary = "$white"
+// textPrimaryInverse = 
+textSecondary = "$grey2"
+// textSecondaryInverse = 
+textDisabled = "$grey3"
+
+// STATES
+error = "$pepper"
+success = "$movistarGreen"
+warning = "$egg"
