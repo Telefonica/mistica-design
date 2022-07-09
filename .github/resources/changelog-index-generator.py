022fcf2a506071b0955e5b30afcfcf4a68f0618b
Yayo <yceballost@gmail.com>
2022-07-09 22:40:30 +0200

    sort by version

--- .github/resources/changelog-index-generator.py
+++ .github/resources/changelog-index-generator.py
@@ -1,4 +1,5 @@
 import os
+from packaging.version import parse as parse_version
 
 BREAK = "\n"
 
@@ -6,26 +7,41 @@ BREAK = "\n"
 dir_path = r'./changelog-versions'
 
 # list to store files
-changelog_files = []
+changelog_files_extension = []
 
 # Iterate directory
 for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
-        changelog_files.append(path)
-
+        changelog_files_extension.append(path)
     if path == ".DS_Store":
-        changelog_files.remove(path)
+        changelog_files_extension.remove(path)
+# print(changelog_files_extension)
+
+# Remove extension from files
+changelog_files = list(
+    map(lambda x: x.replace('.md', ''), changelog_files_extension))
 print(changelog_files)
+
+# Sort files by version
+changelog_files_sorted = sorted(changelog_files, reverse=True, key=lambda x: [int(
+    i) if i.isdigit() else i for i in x.split('.')])
+print(changelog_files_sorted)
 
 changelog_version = ""
 
-for item in sorted(changelog_files, reverse=True):
-    changelog_version += "- " + "[" + item.replace(".md", "") + "]" + \
-        "(" + "changelog-versions/" + item + ")" + BREAK
-    # print(changelog_version)
+for item in sorted(changelog_files_sorted):
+    # print(item)
+    if item.endswith('.0.0'):
+        changelog_version += "- " + "[" + item + "]" + \
+            "(" + "changelog-versions/" + item + ".md" + ")" + BREAK
+    else:
+        changelog_version += "\t" + "- " + "[" + item + "]" + \
+            "(" + "changelog-versions/" + item + ".md" + ")" + BREAK
+        # print(changelog_version)
 
 file_content = "# Changelog index" + BREAK + changelog_version
+# print(file_content)
 
 output_file_path = "./CHANGELOG.md"
 file = open(output_file_path, "w+")
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Changelog index
-- [index](changelog-versions/index.md)
-- [2.0.0](changelog-versions/2.0.0.md)
-- [1.2.0](changelog-versions/1.2.0.md)
-- [1.1.0](changelog-versions/1.1.0.md)
 - [1.0.0](changelog-versions/1.0.0.md)
+	- [1.1.0](changelog-versions/1.1.0.md)
+	- [1.10.1](changelog-versions/1.10.1.md)
+	- [1.2.0](changelog-versions/1.2.0.md)
+	- [1.9.1](changelog-versions/1.9.1.md)
+- [2.0.0](changelog-versions/2.0.0.md)
