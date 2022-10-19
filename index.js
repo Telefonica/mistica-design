7ad62b04f7ac5744a6d271531101929a26d69fec
Yayo <yceballost@gmail.com>
2022-10-19 22:20:17 +0200

    update

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ fetch("https://raw.githubusercontent.com/Telefonica/mistica-design/production/to
 .then(res => res.json())
 .then((res) => {
   const light = res.light;
-  getElement('value').innerHTML =  'Value: ' + light.background.value;
-  getElement('description').innerHTML = 'Description: ' + light.background.description;
+  getElement('name').innerHTML =  'Name: ' + Object.keys(light)[15];
+  getElement('value').innerHTML = 'Value: ' + Object.values(light)[15].value;
+  getElement('description').innerHTML = 'Description: ' + Object.values(light)[15].description;
   // do the rest here
 });
