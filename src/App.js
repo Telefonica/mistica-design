f506c122eb45a4836e112cceb0dfc850d2e2a5e8
Yayo <yceballost@gmail.com>
2022-12-27 12:31:44 +0100

    index

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import '@telefonica/mistica/css/reset.css';
+import {
+  Stack,
+  Box,
+  ResponsiveLayout,
+  DataCard,
+  Circle,
+  IconShopRegular,
+  ButtonLink,
+  Title2,
+  skinVars,
+  useScreenSize,
+  useTheme,
+  
+} from '@telefonica/mistica';
+import React from 'react';
+
+
+  
+const App = (isDesktopOrBigger) => (
+
+  <Box paddingY={isDesktopOrBigger ? 40 : 800}>
+  <ResponsiveLayout>
+      <Stack space={40}>
+        <Title2>Mística Index</Title2>
+        <Stack space={16}>
+      <DataCard
+        icon={
+          <Circle backgroundColor={skinVars.colors.brandLow} size={40}>
+            <IconShopRegular color={skinVars.colors.brand} />
+          </Circle>
+        }
+        title="Mística Wrapper '22'"
+        description="Description"
+        buttonLink={<ButtonLink onPress={() => {}}>Visit</ButtonLink>}
+          />
+          <DataCard
+        icon={
+          <Circle backgroundColor={skinVars.colors.brandLow} size={40}>
+            <IconShopRegular color={skinVars.colors.brand} />
+          </Circle>
+        }
+        title="Tokens Table"
+        description="Description"
+        buttonLink={<ButtonLink onPress={() => {}}>Visit</ButtonLink>}
+          />
+          </Stack>
+    </Stack>
+  </ResponsiveLayout>
+</Box>
+  
+);
+
+
+export default App;
