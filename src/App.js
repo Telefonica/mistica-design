6a6ec099ac0a0e74e392bf4f759f8afba9c2f5c2
Yayo <yceballost@gmail.com>
2022-12-27 13:28:27 +0100

    add dashboard

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = (isDesktopOrBigger) => (
         }
         title="Tokens Table --"
         description="Description"
-        buttonLink={<ButtonLink href="../dashboard/index.html">Visit</ButtonLink>}
+        buttonLink={<ButtonLink href="dashboard/index.html">Visit</ButtonLink>}
           />
           </Stack>
     </Stack>
