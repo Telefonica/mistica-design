fa8798f206bd704e59776b1b14ca3d3cef4e9f84
Alex Bueno <44420072+aweell@users.noreply.github.com>
2023-01-16 13:48:54 +0100

    Add react router + move internal pages to components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import "@telefonica/mistica/css/roboto.css";
 import "@telefonica/mistica/css/reset.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { getTelefonicaSkin, ThemeContextProvider } from "@telefonica/mistica";
-import BodyBackground from "./components/body-background.tsx";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <ThemeContextProvider
       theme={{
-        // colorScheme: "light",
         skin: getTelefonicaSkin(),
         i18n: { locale: "es-ES", phoneNumberFormattingRegionCode: "ES" },
       }}
     >
-      <BodyBackground />
       <App />
     </ThemeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
