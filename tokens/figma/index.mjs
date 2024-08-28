c0981d987f2402b35b37348492528d42e73db5ba
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2024-08-28 19:03:49 +0200

    improve file management

--- tokens/figma/index.mjs
+++ tokens/figma/index.mjs
@@ -9,9 +9,16 @@ dotenv.config({ path: "../../.env" });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const FILE_KEY_1 = process.env.FILE_KEY_1;
-const FILE_KEY_2 = process.env.FILE_KEY_2;
 const FIGMA_TOKEN = process.env.FIGMA_TOKEN;
+
+const FILE_KEYS = {
+  movistar: process.env.MOVISTAR_FILE_KEY,
+  "o2-new": process.env.O2_NEW_FILE_KEY,
+  "vivo-new": process.env.VIVO_NEW_FILE_KEY,
+  telefonica: process.env.TELEFONICA_FILE_KEY,
+  blau: process.env.BLAU_FILE_KEY,
+  tu: process.env.TU_FILE_KEY,
+};
 
 const tokensPath = path.resolve(__dirname, "../");
 
@@ -26,10 +33,14 @@ const jsonData = extractJsonData(
   tokensPath
 );
 
-const brands = {
-  "o2-new": `https://api.figma.com/v1/files/${FILE_KEY_2}/variables`,
-  "vivo-new": `https://api.figma.com/v1/files/${FILE_KEY_1}/variables`,
-};
+const brands = Object.fromEntries(
+  Object.entries(FILE_KEYS).map(
+    ([brand, FILE_KEY]) => [
+      brand,
+      `https://api.figma.com/v1/files/${FILE_KEY}/variables`,
+    ]
+  )
+);
 
 async function updateCollections(url) {
   try {
