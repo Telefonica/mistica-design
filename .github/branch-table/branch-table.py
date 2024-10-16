c48cea26e36a9add24a1331381f8d01e117f3d0e
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2024-10-16 19:14:30 +0200

    get files from projects

--- .github/branch-table/branch-table.py
+++ .github/branch-table/branch-table.py
@@ -2,13 +2,34 @@ import requests
 import pandas as pd
 import os
 
+# Function to get the files from specific Figma projects
+def get_figma_project_files(project_ids, figma_token):
+    headers = {
+        "X-Figma-Token": figma_token
+    }
+    file_keys = []
+    
+    for project_id in project_ids:
+        print(f"Fetching files for project ID: {project_id}")
+        project_files_url = f"https://api.figma.com/v1/projects/{project_id}/files"
+        files_response = requests.get(project_files_url, headers=headers)
+        
+        if files_response.status_code == 200:
+            files = files_response.json().get("files", [])
+            for file in files:
+                file_keys.append(file["key"])
+        else:
+            print(f"Error fetching files for project {project_id}: {files_response.status_code}")
+    
+    return file_keys
+
 # Function to get information from a Figma file, using the branch_data=true parameter
 def get_figma_file_data(file_key, figma_token):
     headers = {
         "X-Figma-Token": figma_token
     }
     url = f"https://api.figma.com/v1/files/{file_key}?branch_data=true"
-    response = requests.get(url, headers=headers, verify=True)
+    response = requests.get(url, headers=headers, verify=False)
 
     if response.status_code == 200:
         return response.json()
@@ -63,80 +84,23 @@ def update_github_issue(issue_number, repo_owner, repo_name, markdown_content, g
         print("Issue updated successfully")
     else:
         print(f"Failed to update issue: {response.status_code} - {response.text}")
-    
-
-# List of Figma file keys to analyze
-file_keys = [
-    "WCkDDzlXE16R6yXaljxddj",
-    "DSWhPLyJzbliP1fBrLxDUR",
-    "w7fBxCsEb8WrMVVuxDnCQd",
-    "JHuzksh01yxExMeMQBvymq",
-    "ObNHOLPtrIytjy9BH7M9jW",
-    "m8srmP3eedfvDaqYnbM6PI",
-    "19IXMaFqdYeC1IIdTwXBgY",
-    "czemeClWRGBI8oF7caNa5m",
-    "CjvgrHEIycSQ6exznxnFXT",
-    "EApRpjaTyUOwW5VQU2ZqgP",
-    "JXy7Y07eb0Axg0ThVRWKju",
-    "jWWCJ9kYl6I5uHLz3GcgRp",
-    "KDxPjcTXq2NCvQZjb4FXgi",
-    "U4ipIXOk64bdM5tSvaqPKS",
-    "gUZfZajV9Zfade0rBYWZQg",
-    "BctaFHf0PVPX2uwRSl8taf",
-    "koROdh3HpEPG2O8jG52Emh",
-    "LzhzNPzE8nY9d7Yvo10d5d",
-    "tKdPOfcUALzVIh5oizFbm7",
-    "hxJvuIQQ1xf50xIOQFUZbQ",
-    "6IN1rHqpYLILjgWMuUGKvJ",
-    "512a5Ke4EldL0GYffCXGCJ",
-    "LhZ5g2KAckJLzBIA1720H0",
-    "XiGe8xqm5OOjUSmdhU4rzA",
-    "DAXzyfp6gf7G9SUsnKgTzJ",
-    "LpjgnU1xpmEzQvlXKwAHmn",
-    "Xm3r8nRNlbAiZNlkaH5YX1",
-    "M8t65SWdTUyM44YjxuzCyu",
-    "3iEvVzfHBtg3sROXBHzkn7",
-    "e8d3PkXitUJ1Jwgc9Y3mNc",
-    "ncQ4GRI3ZEjEtNW6pyROX2",
-    "Be8QB9onmHunKCCAkIBAVr",
-    "KYdCKU8obcDapI8ihpsaWs",
-    "YYWgtxCOpR87Nq5SbSzI8k",
-    "a1pgrI7Wg41iyXWU8ETqlU",
-    "8TXIyWTJ8plJ06i1qwv97k",
-    "4GmsXwcMOapuCa0Atm6E84",
-    "Os5UfsnhLtQ9rnzmtcX8J4",
-    "gmcy7hwB9WXxqT05jBnA3W",
-    "3IO1EsGDeBRiFdF23mrvuj",
-    "RkHDrAELuQIlwNSQBfjSBx",
-    "lqCbeKPfXwOWxSFGOO4bY4",
-    "nxToAYk7FGC86TS0mY61TU",
-    "w7E0mmB92eio0zHw7h9iS2",
-    "UZHm7V3gO7nVnbf1b0Vj7b",
-    "yCHLIfy4WMfRdlADwyL4kZ",
-    "O3uLKJQ1MNsMFXYqmyPTn8",
-    "3ME86VKaAQ2BoeqoQuHMYx",
-    "YIQIeK0R4bQNNkw2Friv6b",
-    "J0LzwXn0tzGNIkgvFnt0Yz",
-    "WtF16bfvE9kKS4yx3VtLuE",
-    "tOb2OmRf6gnEK3fnSWPKOG",
-    "wseConngNv2MYEXgCk5uI4",
-    "f9tAOJproMIiJJuPchDEhG",
-    "81rPCqGchGf54aoh2kpF1o",
-    "MU9VZyeyzTSDPLWmZMGYkK",
-    "yQRVCOmRQNM98bnXndLPxK",
-    "5xDoAS5UTtTPMP8zDsxiMt",
-    "il3GkMh7p85wpXvtWuvLRT",
-    "InXkFcu9cBzKmMjZtKXSy7",
-    "dvMqeg6s8tDh9Hyxu7IYDl",
-    "aNHyvXe1mqjaelE8XWVQD5",
-    "pzcfoIj4osTMiOHoChiCFm",
-    "PApssATUkePZnIOB4GmAeI",
-    # Add more file keys here
-]
 
 # Personal access token for Figma and GitHub APIs
 figma_token = os.getenv("FIGMA_TOKEN")
 github_token = os.getenv("GITHUB_TOKEN")
+
+# List of project IDs to fetch files from
+project_ids = [
+    "25719143", # Mística Libraries
+    "266390224", # Mística Skins Libraries
+    "27955986", # Specs
+    "170790970", # Community Specs
+    "30110755", # Materials
+    # Add more project id here
+]
+
+# Fetch all file keys from the specified Figma projects
+file_keys = get_figma_project_files(project_ids, figma_token)
 
 # Analyze the files and generate the table
 df = analyze_files(file_keys, figma_token)
