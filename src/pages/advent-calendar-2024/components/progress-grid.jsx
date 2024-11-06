2c036f086c8e5137daa79f94edacfe1daef2c382
Alex Bueno <44420072+aweell@users.noreply.github.com>
2024-11-06 21:09:21 +0100

    Improvements in progress view

---
+++ src/pages/advent-calendar-2024/components/progress-grid.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Inline, skinVars, Text4, ThemeVariant } from "@telefonica/mistica";
+
+const ProgressGrid = ({ completedDays }) => {
+  const columns = 8;
+  const daysInNovember = Array.from({ length: 24 }, (_, i) => i + 1);
+
+  // Extract day number from `YYYY-MM-DD` format
+  const completedDayNumbers = completedDays.map((date) =>
+    new Date(date).getDate()
+  );
+
+  const isDayCompleted = (day) => completedDayNumbers.includes(day);
+
+  const dayStyles = (completed) => ({
+    background: completed ? skinVars.colors.brand : skinVars.colors.neutralLow,
+    borderRadius: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 56,
+    height: 56,
+    outline: `1px solid ${skinVars.colors.inverse}`,
+    outlineOffset: "-4px",
+  });
+
+  return (
+    <Inline space={8} wrap>
+      {daysInNovember.map((day) => (
+        <div key={day}>
+          <div style={dayStyles(isDayCompleted(day))}>
+            <ThemeVariant variant={isDayCompleted(day) ? "inverse" : "default"}>
+              <Text4>{day}</Text4>
+            </ThemeVariant>
+          </div>
+        </div>
+      ))}
+    </Inline>
+  );
+};
+
+export default ProgressGrid;
