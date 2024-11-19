e45e742850716ba9889a01acf23fa0be4130fc62
Alex Bueno <44420072+aweell@users.noreply.github.com>
2024-11-19 16:55:06 +0100

    Fix toast issues

--- src/pages/advent-calendar-2024/components/toast-wrapper.jsx
+++ src/pages/advent-calendar-2024/components/toast-wrapper.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import Toast from "./toast"; // Assuming your Toast component is already implemented
+import { Stack } from "@telefonica/mistica";
 
 const ToastWrapper = ({ toasts, removeToast }) => {
+  const totalToasts = toasts.length;
+
   return (
     <div
       style={{
@@ -10,18 +13,28 @@ const ToastWrapper = ({ toasts, removeToast }) => {
         right: "16px",
       }}
     >
-      <div style={{ position: "relative" }}>
-        {toasts.map((toast, index) => (
-          <Toast
-            id={toast.id}
-            key={toast.id}
-            title={toast.name}
-            description={toast.message}
-            icon={toast.icon}
-            onClose={() => removeToast(toast.id)} // Dismiss the toast by `id`
-          />
-        ))}
-      </div>
+      <Stack space={8}>
+        {toasts.map((toast, index) => {
+          const scaleValue = 1 - index * 0.2;
+          console.log(`scale(${scaleValue})`); // Log the scale value
+
+          return (
+            <Toast
+              id={toast.id}
+              key={toast.id}
+              title={toast.name}
+              description={toast.message}
+              icon={toast.icon}
+              onClose={() => removeToast(toast.id)} // Dismiss the toast by `id`
+              style={{
+                right: "16px",
+                zIndex: 1000 + index,
+              }}
+              delay={(totalToasts - index - 1) * 1000} // Last toast will have the longest delay
+            />
+          );
+        })}
+      </Stack>
     </div>
   );
 };
