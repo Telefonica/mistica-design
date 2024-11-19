09518106b3cb579af424c14006e6cbc1497bc2d6
Marina Higueras <mhiguera@student.42madrid.com>
2024-11-19 13:29:33 +0100

    updated content config

--- src/pages/advent-calendar-2024/utils/content-config.jsx
+++ src/pages/advent-calendar-2024/utils/content-config.jsx
@@ -5,21 +5,47 @@ import { IllustrationWoolClothes, IllustrationWishesLetter } from "../assets/ill
 
 const contentByDate = {
     "2024-11-01": "Welcome to the Advent Calendar!",
-    "2024-11-02": <GamesView game="Memory" />,
-    "2024-11-03": <GamesView game="Wordle" />,
-    "2024-11-04": <GamesView game="Candy" />,
+    
+    "2024-11-02": {
+      illustration: <IllustrationWishesLetter />,
+      content: <GamesView game="Memory" />,
+      title: "Memory Cards",
+      description: "Play Memory Cards!",
+    },
 
-    "2024-11-06": <QuizView 
+    "2024-11-03": {
+      illustration: <IllustrationWishesLetter />,
+      content: <GamesView game="Wordle" />,
+      title: "Wordle",
+      description: "Play Wordle!",
+    },
+
+    "2024-11-04": {
+    illustration: <IllustrationWishesLetter />,
+    content: <GamesView game="Candy" />,
+    title: "Candy Crush",
+    description: "Play Candy Crush!",
+  },
+
+    "2024-11-06": {
+    illustration: <IllustrationWoolClothes />,
+    content: <QuizView 
     questionTitle={quizQuestions[0].questionTitle}
     options={quizQuestions[0].options}
-    correctAnswer={quizQuestions[0].correctAnswer}
-  />,
+    correctAnswer={quizQuestions[0].correctAnswer}/>,
+    title: "Quiz",
+    description: "Test your knowledge of Mística!",
+    },
 
-  "2024-11-07": <QuizView 
+  "2024-11-07": {
+    illustration: <IllustrationWishesLetter />,
+    content: <QuizView 
     questionTitle={quizQuestions[1].questionTitle}
     options={quizQuestions[1].options}
-    correctAnswer={quizQuestions[1].correctAnswer}
-  />,
+    correctAnswer={quizQuestions[1].correctAnswer}/>,
+    title: "Quiz",
+    description: "Test your knowledge of Mística!",
+  },
 
   "2024-11-11": {
     illustration: <IllustrationWishesLetter />,
@@ -35,8 +61,7 @@ const contentByDate = {
     content: <QuizView 
     questionTitle={quizQuestions[2].questionTitle}
     options={quizQuestions[2].options}
-    correctAnswer={quizQuestions[2].correctAnswer}
-  />,
+    correctAnswer={quizQuestions[2].correctAnswer}/>,
     title: "Movie Night",
     description: "Watch a movie with your family or friends.",
   },
