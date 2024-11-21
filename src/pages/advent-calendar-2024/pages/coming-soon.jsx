7c3f76e1f5f40142a3fe98934f8e45bf643c3cd0
andredelramo-hm <andrea@hamon.es>
2024-11-21 14:42:09 +0100

    update-coming-soon

--- src/pages/advent-calendar-2024/pages/coming-soon.jsx
+++ src/pages/advent-calendar-2024/pages/coming-soon.jsx
@@ -1,10 +1,10 @@
 import { 
   ResponsiveLayout, 
-  Text5, 
+  Text7, 
   Box, 
   Stack, 
   Text, 
-  ButtonPrimary, 
+  Align,
   Timer, 
 } from "@telefonica/mistica";
 import NavBar from "../components/navbar";
@@ -16,27 +16,29 @@ const endTimestamp = new Date(defaultTargetDate).getTime();
 
   return (
     <>
-      <NavBar /> 
-
-      <ResponsiveLayout>
-        <Box paddingY={42}>
-          <Stack space={48}>
-            <Stack space={0}>
-              
-              <Text size={60} weight="medium">
-                Coming Soon
-              </Text>
-              <Timer
-                  endTimestamp={endTimestamp}
-                  minTimeUnit="seconds"
-                  maxTimeUnit="days"
-                  themeVariant="default"
-                  boxed={true}
-                />
-            </Stack>
-          </Stack>
-        </Box>
-      </ResponsiveLayout>
+      <ResponsiveLayout
+        variant="inverse">
+        <NavBar /> 
+        <Align x="center" y="center" height="calc(100vh - 138px)">
+        <div style={{maxWidth:600}}>
+            <Box paddingY={42}>
+              <Stack space={48}>
+                <Stack space={24} >
+                    <Text7 size={60} weight="medium">Mística Advent Calendar</Text7>
+                    <Text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dolor dolorem sed ipsum vitae itaque, perferendis odit tempore delectus quidem tempora nihil illum est nam eius dolorum hic nulla quia.</Text>
+                    <Timer
+                        endTimestamp={endTimestamp}
+                        minTimeUnit="seconds"
+                        maxTimeUnit="days"
+                        themeVariant="inverse"
+                        boxed={false}
+                      />
+                </Stack>
+              </Stack>
+            </Box>
+        </div>
+        </Align>
+        </ResponsiveLayout>
     </>
   );
 };
