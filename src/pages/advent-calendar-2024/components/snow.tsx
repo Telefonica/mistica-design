cb2d9dead543181942f25b59008c4f1a8e52f6ee
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2024-11-24 14:05:02 +0100

    removing mobile logic in snow

--- src/pages/advent-calendar-2024/components/snow.tsx
+++ src/pages/advent-calendar-2024/components/snow.tsx
@@ -7,7 +7,6 @@ import { useScreenSize } from "@telefonica/mistica";
 
 const Snow = () => {
   const [images, setImages] = useState<HTMLImageElement[]>([]);
-  const isMobile = useScreenSize();
 
   useEffect(() => {
     let isMounted = true; // Controla si el componente sigue montado
@@ -56,9 +55,9 @@ const Snow = () => {
         zIndex: "9999999",
       }}
       color="#EEF0FB"
-      radius={isMobile ? [30, 30] : [50, 50]}
+      radius={[45, 45]}
       speed={[1.0, 2.0]} // Velocidad de los copos
-      snowflakeCount={isMobile ? 8 : 20}
+      snowflakeCount={20}
       images={images} // Pasamos las imágenes cargadas al componente
     />
   );
