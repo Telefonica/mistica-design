02f9c44f09516330e6b0e8648ef3b21231885224
Alex Bueno <44420072+aweell@users.noreply.github.com>
2025-03-12 18:41:31 +0100

    Fixes from CR

--- tokens/figma/variables.mjs
+++ tokens/figma/variables.mjs
@@ -14,7 +14,7 @@ export const FONT_FAMILIES = {
   [BRANDS.TELEFONICA]: "Telefonica Sans",
   [BRANDS.BLAU]: "SF Pro Text",
   [BRANDS.TU]: "Telefonica Sans",
-  [BRANDS.ESIMFLAG]: "Telefonica Sans",
+  [BRANDS.ESIMFLAG]: "On Air",
 };
 
 export const ICON_SETS = {
@@ -34,7 +34,7 @@ export const BRAND_NAMES = {
   [BRANDS.TELEFONICA]: "Telefónica",
   [BRANDS.BLAU]: "Blau",
   [BRANDS.TU]: "TU",
-  [BRANDS.ESIMFLAG]: "Esimflag",
+  [BRANDS.ESIMFLAG]: "eSimFLAG",
 };
 
 export const getPaletteVariables = (
